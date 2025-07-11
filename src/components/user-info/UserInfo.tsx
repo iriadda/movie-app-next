4b143585f98b88f37b65808e8c747bd22709031e
iriadda <iriadda@gmail.com>
2025-07-11 20:41:20 +0300

    README.md

--- src/components/user-info/UserInfo.tsx
+++ src/components/user-info/UserInfo.tsx
@@ -4,6 +4,7 @@ const UserInfo = () => {
     return (
         <>
             <a href={'#'} className="user-info">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img src={`https://img.icons8.com/color/48/user-female-circle--v1.png`} alt={"user-female-circle--v1"}
                      width="48" height="48"/>
                 <p className="username">User name</p>
